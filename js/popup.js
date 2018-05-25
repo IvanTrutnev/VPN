ffcf62c654b09d66fa285c2a0a75365283eccb83
Ivan Trutnev <i@gmail.com>
2018-05-25 17:21:04 +0300

    vpn popup

--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,3 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const btn = document.getElementById('test');
-  btn.addEventListener('click', () => {
-    //chrome.runtime.sendMessage({ currency: 'asd' })
-  });
+
 });
